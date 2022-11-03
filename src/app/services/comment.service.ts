98fb02a58c11b0e8f68d703519930efcfc554765
Nazar Sergeev <privat.nazar@gmail.com>
2022-11-03 17:47:10 +0200

    lesson 1

---
+++ src/app/services/comment.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+
+import {IComment} from "../interfaces/comment.interface";
+import {urls} from "../configs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  constructor(private httpClient:HttpClient) { }
+
+  getAll():Observable<IComment[]>{
+    return this.httpClient.get<IComment[]>(urls.comments)
+  }
+}
